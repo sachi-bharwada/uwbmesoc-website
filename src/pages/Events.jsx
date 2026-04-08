bcf99e3731dff9e09216eb66566af6b1d1655fd8
sachi-bharwada <smbharwa@uwaterloo.ca>
2026-04-07 22:49:57 -0700

    updated events

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,7 +1,12 @@
+import './Events.css';
+
 function Events() {
   return (
-    <div>
-      <h1>Events</h1>
+    <div className="events-container">
+      <h1 className="events-title">Events</h1>
+      <p className="events-blurb">
+        Visit this page for our upcoming events, which will be posted soon!
+      </p>
     </div>
   );
 }
